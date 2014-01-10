17b92403a2e29e6a3b36251f9c02f33cec924d30
Benoit Jehanno <jehannob@ensipc13.imag.fr>
2014-01-10 11:24:41 +0100

    vim moi ca

--- TP1-Mocap/characterAnimNonDep/skeleton.cpp
+++ TP1-Mocap/characterAnimNonDep/skeleton.cpp
@@ -1,6 +1,7 @@
 #include "skeleton.h"
 #include <skeletonIO.h>
 #include <qglviewer.h>
+#include <math.h>
 
 
 using namespace std;
@@ -124,12 +125,98 @@ void Skeleton::animate(int iframe)
 
 void Skeleton::eulerToMatrix(double rx, double ry, double rz, int rorder, glm::mat3 *R)
 {
-	glm::mat3 mx, my, mz;
+  glm::mat3 mx(1, 0,       0,
+	       0, cos(rx), -sin(rx),
+	       0, sin(rx), cos(rx));
 
+  glm::mat3 my(cos(ry),  0, sin(ry),
+	       0,        1, 0,
+	       -sin(ry), 0, cos(ry));
+
+  glm::mat3 mz(cos(rz),  -sin(ry), 0,
+	       sin(rz),  cos(rz),  0,
+	       0,        0,        1);
+
+  switch (rorder) {
+  case roZYX :
+    *R = mx*my*mz;
+    break;
+  case roXZY :
+    *R = my*mz*mx;
+    break;
+  case roYXZ :
+    *R = mz*mx*my;
+    break;
+  case roYZX :
+    *R = mx*mz*my;
+    break;
+  case roZXY :
+    *R = my*mx*mz;
+    break;
+  case roXYZ :
+    *R = mz*my*mx;
+    break;
+  default :
+    *R = mx*my*mz;
+  }
+  
 }
+
+inline float SIGN(float x) {return (x >= 0.0f) ? +1.0f : -1.0f;}
+inline float NORM(float a, float b, float c, float d) {return sqrt(a * a + b * b + c * c + d * d);}
+
 void Skeleton::matrixToQuaternion(glm::mat3 R, qglviewer::Quaternion *q)
 {
-	
+  
+  q0 = ( r11 + r22 + r33 + 1.0f) / 4.0f;
+  q1 = ( r11 - r22 - r33 + 1.0f) / 4.0f;
+  q2 = (-r11 + r22 - r33 + 1.0f) / 4.0f;
+  q3 = (-r11 - r22 + r33 + 1.0f) / 4.0f;
+
+  if(q0 < 0.0f) q0 = 0.0f;
+  if(q1 < 0.0f) q1 = 0.0f;
+  if(q2 < 0.0f) q2 = 0.0f;
+  if(q3 < 0.0f) q3 = 0.0f;
+
+  q0 = sqrt(q0);
+  q1 = sqrt(q1);
+  q2 = sqrt(q2);
+  q3 = sqrt(q3);
+
+  if(q0 >= q1 && q0 >= q2 && q0 >= q3) {
+    q0 *= +1.0f;
+    q1 *= SIGN(r32 - r23);
+    q2 *= SIGN(r13 - r31);
+    q3 *= SIGN(r21 - r12);
+
+  } else if(q1 >= q0 && q1 >= q2 && q1 >= q3) {
+    q0 *= SIGN(r32 - r23);
+    q1 *= +1.0f;
+    q2 *= SIGN(r21 + r12);
+    q3 *= SIGN(r13 + r31);
+
+  } else if(q2 >= q0 && q2 >= q1 && q2 >= q3) {
+    q0 *= SIGN(r13 - r31);
+    q1 *= SIGN(r21 + r12);
+    q2 *= +1.0f;
+    q3 *= SIGN(r32 + r23);
+
+  } else if(q3 >= q0 && q3 >= q1 && q3 >= q2) {
+    q0 *= SIGN(r21 - r12);
+    q1 *= SIGN(r31 + r13);
+    q2 *= SIGN(r32 + r23);
+    q3 *= +1.0f;
+
+  } else {
+    std::cerr << "coding error\n" << std::endl;
+  }
+
+  r = NORM(q0, q1, q2, q3);
+  q0 /= r;
+  q1 /= r;
+  q2 /= r;
+  q3 /= r;
+  
 }
 void Skeleton::quaternionToAxisAngle(qglviewer::Quaternion q, qglviewer::Vec *vaa)
 {
